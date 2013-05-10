9f543e751ae9c7c36b08b0715815bcd8e48cf027
Sven <sven.elfgren@gmail.com>
2013-05-10 13:35:01 +0200

    removed unused gui

--- src/clientpackage/ClientGUI.java
+++ src/clientpackage/ClientGUI.java
@@ -21,46 +21,37 @@ public class ClientGUI extends JFrame implements ActionListener {
 	private JLabel label;
 	// to hold the Username and later on the messages
 	private JTextField tf;
-	// to hold the server address an the port number
-	private JTextField tfServer, tfPort;
 	// to Logout and get the list of the users
-	private JButton login, logout, whoIsIn;
+	private JButton login, logout;
 	// for the chat room
 	private JTextArea ta;
 	// if it is for connection
 	private boolean connected;
-	// the Client object
-	//private Client client;
-	// the default port number
-	private int defaultPort;
-	private String defaultHost;
 	
 	private DefaultListModel nameListModel;
 	private JList nameList;
 
 	// Constructor connection receiving a socket number
-	ClientGUI(String host, int port) {
+	ClientGUI() {
 
 		super("Chat Client");
-		defaultPort = port;
-		defaultHost = host;
 		
-		// The NorthPanel with:
-		JPanel northPanel = new JPanel(new GridLayout(3,1));
+		// The northPanel which is the chat room
+		ta = new JTextArea("Welcome to the Chat room\n", 80, 80);
+		JPanel northPanel = new JPanel(new GridLayout(1,1));
+		northPanel.add(new JScrollPane(ta));
+		ta.setEditable(false);
+		
+		// The centerPanel with:
+		JPanel centerPanel = new JPanel(new GridLayout(3,1));
 		// the Label and the TextField
 		label = new JLabel("Enter your username below", SwingConstants.CENTER);
-		northPanel.add(label);
+		centerPanel.add(label);
 		tf = new JTextField("Anonymous");
 		tf.setBackground(Color.WHITE);
-		northPanel.add(tf);
-		add(northPanel, BorderLayout.NORTH);
+		centerPanel.add(tf);
 
-		// The CenterPanel which is the chat room
-		ta = new JTextArea("Welcome to the Chat room\n", 80, 80);
-		JPanel centerPanel = new JPanel(new GridLayout(1,1));
-		centerPanel.add(new JScrollPane(ta));
-		ta.setEditable(false);
-		add(centerPanel, BorderLayout.CENTER);
+		
 		
 		//east namepanel
 		JLabel users = new JLabel("Online Users", SwingConstants.CENTER);
@@ -74,7 +65,6 @@ public class ClientGUI extends JFrame implements ActionListener {
 		eastpanel.setLayout(new BorderLayout(2, 1));
 		eastpanel.add(users,BorderLayout.NORTH);
 		eastpanel.add(p1, BorderLayout.CENTER);
-		add(eastpanel,BorderLayout.EAST);
 
 		// the 3 buttons
 		login = new JButton("Login");
@@ -84,8 +74,14 @@ public class ClientGUI extends JFrame implements ActionListener {
 		logout.setEnabled(false);		// you have to login before being able to logout
 
 		JPanel southPanel = new JPanel();
-		southPanel.add(login);
-		southPanel.add(logout);
+		southPanel.setLayout(new BorderLayout(1, 2));
+		southPanel.add(login, BorderLayout.WEST);
+		southPanel.add(logout, BorderLayout.EAST);
+		
+		add(centerPanel, BorderLayout.NORTH);
+		add(northPanel, BorderLayout.CENTER);
+		
+		add(eastpanel,BorderLayout.EAST);
 		add(southPanel, BorderLayout.SOUTH);
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -107,12 +103,6 @@ public class ClientGUI extends JFrame implements ActionListener {
 		logout.setEnabled(false);
 		label.setText("Enter your username below");
 		tf.setText("Anonymous");
-		// reset port number and host name as a construction time
-		tfPort.setText("" + defaultPort);
-		tfServer.setText(defaultHost);
-		// let the user change them
-		tfServer.setEditable(false);
-		tfPort.setEditable(false);
 		// don't react to a <CR> after the username
 		tf.removeActionListener(this);
 		connected = false;
@@ -125,7 +115,7 @@ public class ClientGUI extends JFrame implements ActionListener {
 		Object o = e.getSource();
 		// if it is the Logout button
 		if(o == logout) {
-		//	client.sendMessage(new ChatMessage(ChatMessage.LOGOUT, ""));
+			//TODO send logout message to server.
 			return;
 		}
 		// ok it is coming from the JTextField
@@ -142,23 +132,23 @@ public class ClientGUI extends JFrame implements ActionListener {
 			String username = tf.getText().trim();
 			// empty username ignore it
 			if(username.length() == 0)
-				return;
+				username = "guest";
 			
 			// disable login button/
 				login.setEnabled(false);
 			// enable the 2 buttons
 			logout.setEnabled(true);
-
-			// disable the Server and Port JTextField
+			
+			//TODO Send login request to server.
 			// Action listener for when the user enter a message
-		tf.addActionListener(this);
+			tf.addActionListener(this);
 		}
 
 	}
 
 	// to start the whole thing the server
 	public static void main(String[] args) {
-		new ClientGUI("localhost", 1500);
+		new ClientGUI();
 	}
 	
 	private void UpdateNameList() {
@@ -168,8 +158,7 @@ public class ClientGUI extends JFrame implements ActionListener {
         	nameListModel.addElement(name);
         }
 	}
-
-}
+	
 
 /**
  * A class that listens for clicks in the name list.
@@ -184,11 +173,14 @@ class NameSelectionListener implements ListSelectionListener {
 	 *            The selected list item.
 	 */
 	public void valueChanged(ListSelectionEvent e) {
-//		if (nameList.isSelectionEmpty()) {
-//			return;
-//		}
-//		String Name = (String) nameList.getSelectedValue();
-//		fillNameList(name);
+		if (nameList.isSelectionEmpty()) {
+			return;
+		}
+		String Name = (String) nameList.getSelectedValue();
+		//TODO send chatroom request to server with wanted chatpartner "Name"
 	}
 }
+
+}
+
 
